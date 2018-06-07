382cf8cc9dfddbf827694a0cb822cd1ec0cf2cad
iltenma <36898842+iltenma@users.noreply.github.com>
2018-06-07 10:55:16 +0200

    Add files via upload

---
+++ practica_10Abril.ino
@@ -0,0 +1,34 @@
+#include <Ultrasonido.h>
+
+
+Ultrasonido sensorUltrasonido (12, 11, 50); //trigger, echo, centimetros
+
+
+int distanciaSensor = 0; //HC - SR04
+int valorMaxSonar = 0; // sensor MAXSonar
+int FSR = 0; // FSR
+
+void setup() {
+Serial.begin (9600);
+}
+
+
+
+void loop() {
+  valorMaxSonar = analogRead (0);
+  distanciaSensor = sensorUltrasonido.Distancia ();
+  FSR = analogRead (1);
+  Serial.print ("SensorUltra ");
+ Serial.println (distanciaSensor);
+   
+   Serial.print ("MaxSensor ");
+  Serial.println (valorMaxSonar);
+  
+  Serial.print ("SensorFSR ");
+ Serial.println (FSR);
+ 
+  delay (200);
+
+  
+
+} 
